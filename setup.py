9338bc1568c8a546281c35b92ea8581c67b22814
Tho Nguyen <tho.nguyen@asterist.com>
2020-02-14 14:50:09 +0900

    added test with python 3.8

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup_kwargs = dict(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
 
